2e61d54086e2ecf31867500b7b11d1c8ae57b516
Gonzalo Guerra <lalosonido@yahoo.com.ar>
2021-06-23 17:24:51 -0300

    ...

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -103,7 +103,7 @@ class Home extends BaseController
             case 'approved':
                 return redirect()->to('aprobada/'.$venta['id_venta']);
                 break;
-            case 'pending':
+            case 'in-proccess':
                 return redirect()->to('pendiente/'.$venta['id_venta']);
                 break;
             case 'rejected':
