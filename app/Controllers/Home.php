5dbe6def1495163ec69c30695292b548d39692d7
Gonzalo Guerra <lalosonido@yahoo.com.ar>
2021-06-23 16:03:16 -0300

    ...

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -122,7 +122,7 @@ class Home extends BaseController
 
         $uri = $this->request->uri;
 
-        echo $this->request->getUri()->getSegments();
+        echo var_dump($this->request->getUri()->getSegments());
 
         die();
 
