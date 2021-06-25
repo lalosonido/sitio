003e61cc79d1b1f242baf4dead048026ebc27377
Gonzalo Guerra <lalosonido@yahoo.com.ar>
2021-06-25 11:50:04 -0300

    ...

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -202,7 +202,7 @@ class Home extends BaseController
 
         $model = new VentaModel();
         $venta = $model->where('preference_id', $this->request->getGet('preference_id'))->find();
-        echo $this->db->getLastQuery();
+        echo $model->getLastQuery();
 
         $data = [];
         $data['id_venta'] = $venta['id_venta'];
